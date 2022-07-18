f960140986269fbb0cd82ef5cd33dc9b173bf595
Юлия Алексина <ulchez@mail.ru>
2022-07-18 23:45:00 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение без знаков препинания и мы найдем самое длинное слово");
+            string str = Console.ReadLine();
+            string[] strArray = str.Split();
+            int max = 0;
+            foreach (string s in strArray)
+            {
+                if (s.Length > max)
+                {
+                    max = s.Length;
+                }
+            }
+            foreach (string s in strArray)
+            {
+                if (s.Length == max)
+                {
+                    Console.WriteLine(s); 
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
